0227df1dfdfbd65a149f4da70b21d671c17f9c99
Austin Almond <aust.almond@gmail.com>
2023-10-28 19:11:32 -0400

    Render an isometric grid

--- grid/ground.gd
+++ grid/ground.gd
@@ -1,6 +1,28 @@
 @tool
-
-class_name Ground extends Node
+class_name Ground extends Node2D
 
 @export_range(0, 20) var width: int = 1
 @export_range(0, 20) var height: int = 1
+
+@onready var tiles: Dictionary = {}
+
+const R_TILE = preload("res://grid/tile.tscn")
+
+
+func _ready():
+	for longitude in range(0, width):
+		for latitude in range(0, height):
+			var tile = R_TILE.instantiate()
+			tile.latitude = latitude
+			tile.longitude = longitude
+			tile.elevation = 0
+			tiles[str(tile)] = tile
+			add_child(tile)
+			tile.set_owner(self)
+	print("> Ground ready")
+
+func clear_tiles():
+	for tile in tiles.values():
+		remove_child(tile)
+		tile.queue_free()
+	tiles = {}
--- grid/tile.gd
+++ grid/tile.gd
@@ -6,31 +6,47 @@ class_name Tile extends Sprite2D
 @export var longitude: int : get = _get_longitude, set = _set_longitude
 @export var elevation: int : get = _get_elevation, set = _set_elevation
 
-@onready var _coord: Coord = Coord.new(0, 0, 0)
+var _latitude: int
+var _longitude: int
+var _elevation: int
 
+
+func _ready():
+	_update_position()
+	print(">%s ready" % self)
+
+func _init(init_latitude: int = 0, init_longitude: int = 0, init_elevation: int = 0) -> void:
+	_latitude = init_latitude
+	_longitude = init_longitude
+	_elevation = init_elevation
+
+func _to_string() -> String:
+	return "Tile@(%d,%d,%d)" % [latitude, longitude, elevation]
 
 func _get_elevation() -> int:
-	return _coord.elevation
+	return _elevation
 
 func _get_latitude() -> int:
-	return _coord.latitude
+	return _latitude
 
 func _get_longitude() -> int:
-	return _coord.longitude
+	return _longitude
 
 func _set_elevation(value: int) -> void:
-	_coord.elevation = value
+	_elevation = value
 	_update_position()
 
 func _set_latitude(value: int) -> void:
-	_coord.latitude = value
+	_latitude = value
 	_update_position()
 
 func _set_longitude(value: int) -> void:
-	_coord.longitude = value
+	_longitude = value
 	_update_position()
 
 func _update_position() -> void:
-	var pos_vector3 = _coord.to_vector3()
+	var coord = Coord.new(latitude, longitude, elevation)
+	var pos_vector3 = coord.to_vector3()
 	position = Vector2(pos_vector3.x, pos_vector3.y)
 	z_index = pos_vector3.z
+	print("%s is at position %s"  % [self, position])
