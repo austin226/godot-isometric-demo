446c68ace11796cb97dff1f0d73908be874dc149
Austin Almond <aust.almond@gmail.com>
2023-10-28 20:04:29 -0400

    Move layers on focus change

--- grid/grid.gd
+++ grid/grid.gd
@@ -3,20 +3,22 @@ class_name Grid extends Node2D
 
 @export var focused_layer: int : get = _get_focused_layer, set = _set_focused_layer
 
-@onready var _game_state: GameState = $"/root/G_GameState"
-
 var _focused_layer: int = 0
 
 var _min_layer: int = 0
 var _max_layer: int = 0
 
+const EXTRA_LAYER_GAP_DOWN = Coord.COORDINATE_SCALE * 5
+const EXTRA_LAYER_GAP_UP = Coord.COORDINATE_SCALE * 7
+
 
 func _ready():
-	_game_state.focus_layer_changed.connect(_on_focus_layer_changed)
+	if not Engine.is_editor_hint():
+		$"/root/G_GameState".focus_layer_changed.connect(_on_focus_layer_changed)
 	for child in get_children():
 		if child is GridLayer:
 			var layer = child as GridLayer
-			layer.position.y = -2 * Coord.COORDINATE_SCALE * layer.layer_number
+			layer.position.y = _default_layer_position_y(layer.layer_number)
 			_min_layer = min(layer.layer_number, _min_layer)
 			_max_layer = max(layer.layer_number, _max_layer)
 			print("Layer %d is at %s" % [layer.layer_number, layer.position])
@@ -32,15 +34,24 @@ func _get_sorted_layers() -> Array:
 		if child is GridLayer:
 			result.append(child)
 	result.sort_custom(_sort_layers)
-	print(result)
 	return result
-
 
 func _on_focus_layer_changed(layer_delta: int) -> void:
 	focused_layer = clamp(focused_layer + layer_delta, _min_layer, _max_layer)
 
+func _default_layer_position_y(layer_number: int) -> float:
+	return -2 * Coord.COORDINATE_SCALE * layer_number
+
 func _set_focused_layer(value: int) -> void:
 	_focused_layer = value
+	for layer in _get_sorted_layers():
+		var y = _default_layer_position_y(layer.layer_number)
+		if layer.layer_number < _focused_layer:
+			y += EXTRA_LAYER_GAP_DOWN
+		elif layer.layer_number > _focused_layer:
+			y -= EXTRA_LAYER_GAP_UP
+		layer.position.y = y
+
 	print("Focused layer: %d" % value)
 
 func _sort_layers(a: GridLayer, b: GridLayer) -> bool:
